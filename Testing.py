5fb629c5fa288e251944904705e839932d248444
Yousuf El-Sherif <yelshrif33@gmail.com>
2025-02-26 01:07:30 +0300

    sdfsdf

--- Testing.py
+++ Testing.py
@@ -1,9 +1,6 @@
-print('hello world')
-<<<<<<< HEAD
+
 print('you smell like a monkey')
 print('From adrian branch')
 
 
-=======
-print('you smell like a monkey')
->>>>>>> parent of 343650c (add to testing.py the name of the branch)
+
